6f6c9f90c53c537e7ff011b81de6b6041c2b1335
mdeleonbalbiani <martinadeleonbalbiani@gmail.com>
2021-07-07 11:58:18 -0300

    Carga de Clase 6

--- Clase6/arrays.js
+++ Clase6/arrays.js
@@ -1,31 +1,18 @@
-
 class Promedio{
-    datos(primerEscrito, primerParcial, segundoEscrito, segundoParcial, tercerEscrito, faltas) {
+    datos(nombre, primerEscrito, primerParcial, segundoEscrito, segundoParcial, tercerEscrito, faltas) {
+        this.nombre = nombre;
         this.primerEscrito = primerEscrito;
         this.primerParcial = primerParcial;
         this.segundoEscrito = segundoEscrito;
         this.segundoParcial = segundoParcial;
         this.tercerEscrito = tercerEscrito;
         this.faltas = faltas;
-        this.promedioFinal = promedio(primerParcial, segundoParcial, primerEscrito, segundoEscrito, tercerEscrito);
+        this.promedioFinal;
     }
     calculoPromedio(primerParcial, segundoParcial, primerEscrito, segundoEscrito, tercerEscrito){
         let promedioParciales = (primerParcial + segundoParcial)*0.75;
         let promedioEscritos = (primerEscrito + segundoEscrito + tercerEscrito)*0.25;
         this.promedioFinal = (promedioEscritos + promedioParciales)/2;
-    }
-    devolucion(promedioFinal, faltas) {
-        if (faltas < 22) {
-            if (promedioFinal < 7 && promedioFinal >= 4) {
-                alert("El alumno debe rendir examen en diciembre");
-            } else if (promedioFinal < 4) {
-            alert("El alumno debe rendir examen en febrero");
-            } else if (promedioFinal > 7){
-                alert("El alumno aprobó la materia con nota " + promedioFinal);
-            }
-        }else{
-            alert("El alumno debe recursar la materia, no llego a las asistencias minimas necesarias");
-        }
     }
 }
 
@@ -44,7 +31,32 @@ function validacionNotas(dato){
     return dato;
 }
 
+function devolucion(faltas, promedioFinal) {
+    if (faltas < 22) {
+        if (promedioFinal < 7 && promedioFinal >= 4) {
+            alert("El alumno debe rendir examen en diciembre");
+        } else if (promedioFinal < 4) {
+        alert("El alumno debe rendir examen en febrero");
+        } else if (promedioFinal > 7){
+            alert("El alumno aprobó la materia con nota " + promedioFinal);
+        }
+    }else{
+        alert("El alumno debe recursar la materia, no llego a las asistencias minimas necesarias");
+    }
+}
+
+//Definicion del array
+let alumnos = [];
+//Funcion para agregar dato al array
+function agregar(){
+    alumnos.push(nuevoAlumno);
+    console.log(alumnos);
+}
+
+
+
 //Ingreso de datos
+let nombre = prompt("Ingrese el nombre del alumno");
 let primerEscrito = parseInt(prompt("Ingrese la nota del primer escrito"));
 primerEscrito = validacionNotas(primerEscrito);
 let primerParcial = parseInt(prompt("Ingrese la nota del primer parcial"));
@@ -58,9 +70,11 @@ tercerEscrito = validacionNotas(tercerEscrito);
 let faltas = parseInt(prompt("Ingrese la cantidad de faltas que tuvo el alumno"));
 faltas = validacionFaltas(faltas);
 
-const alumno1 = new Promedio (primerEscrito, primerParcial, segundoEscrito, segundoParcial, tercerEscrito, faltas);
-alumno1.calculoPromedio(primerEscrito, primerParcial, segundoEscrito, segundoParcial, tercerEscrito);
-//Devoluciones al user
-alert("El promedio final del alumno es: " + alumno1.promedioFinal);
-alumno1.devolucion(alumno1.promedioFinal, alumno1.faltas);
+nuevoAlumno = new Promedio(nombre, primerEscrito, primerParcial, segundoEscrito, segundoParcial, tercerEscrito, faltas);
+nuevoAlumno.calculoPromedio(primerEscrito, primerParcial, segundoEscrito, segundoParcial, tercerEscrito);
+agregar();
 
+
+//Devoluciones al user
+alert("El promedio final del alumno es: " + nuevoAlumno.promedioFinal);
+devolucion(nuevoAlumno.faltas, nuevoAlumno.promedioFinal);
